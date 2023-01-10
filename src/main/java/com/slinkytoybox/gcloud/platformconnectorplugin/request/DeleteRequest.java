e5cf5021e348cc39356116c73d701e9b467c8230
Michael Junek <michael@juneks.com.au>
2023-01-10 15:54:34 +1100

    Initial Commit

---
+++ src/main/java/com/slinkytoybox/gcloud/platformconnectorplugin/request/DeleteRequest.java
@@ -0,0 +1,33 @@
+/*
+ *   platformconnectorplugin - DeleteRequest.java
+ *
+ *   Copyright (c) 2022-2023, Slinky Software
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Affero General Public License as
+ *   published by the Free Software Foundation, either version 3 of the
+ *   License, or (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Affero General Public License for more details.
+ *
+ *   A copy of the GNU Affero General Public License is located in the 
+ *   AGPL-3.0.md supplied with the source code.
+ *
+ */
+package com.slinkytoybox.gcloud.platformconnectorplugin.request;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ *
+ * @author Michael Junek (michael@juneks.com.au)
+ */
+@Data
+@EqualsAndHashCode(callSuper=true)
+public class DeleteRequest extends PluginRequest {
+    private String objectId;
+}
